0eced1efe1760e2cb1491a9dd912f0d80165fb44
Austin Chan <austinchan09128@gmail.com>
2024-08-23 13:38:54 +0000

    whatever

--- client/src/App.js
+++ client/src/App.js
@@ -10,24 +10,24 @@ import ProjectDetail from './pages/ProjectDetail';
 
 function App() {
   return (
-      <Router>
-          <Routes>
-              {/* Routes with Navbar */}
-              <Route path="/" element={<><Navbar /><Home /></>} />
-              <Route path="/about" element={<><Navbar /><About /></>} />
-              <Route path="/login" element={<><Navbar /><Login /></>} />
-              <Route path="/projects" element={<><Navbar /><Projects /></>} />
-              <Route path="/projects/:projectName" element={<><Navbar /><ProjectDetail /></>} />
-              
-              {/* Route without Navbar */}
-
-              {/* Unknown url */}
-              <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
-      </Router>
+    <Router>
+      {/* Navbar outside the Routes */}
+      <Navbar />
+      
+      <Routes>
+        {/* Routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/:projectName" element={<ProjectDetail />} />
+        
+        {/* Unknown url */}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
 
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -16,11 +16,12 @@ const Navbar = () => {
         <h1 className="logo">HopeKCC</h1>
         <ul className="nav-links">
           <li><Link to="/">Home</Link></li>
-          <li><Link to="/Projects">Projects</Link></li>
           <li><Link to="/Login">Login</Link></li>
           <li><Link to="/About">About</Link></li>
-          <li><a href="https://paypal.com" target="_blank" rel="noopener noreferrer">Donate</a></li>        
-        </ul>
+	  <li><a href="https://www.paypal.com/donate/?hosted_button_id=YM8LC8PLAFXA4" target="_blank" rel="noopener noreferrer">Donate</a></li>
+              
+
+	  </ul>
       
         <div onClick={handleNav} className="menu-icon">
           {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
@@ -29,7 +30,6 @@ const Navbar = () => {
           <h1 className="mobile-logo">HopeKCC</h1>
           <ul className="mobile-nav-links">
             <li><Link to="/" onClick={handleNav}>Home</Link></li>
-            <li><Link to="/Projects" onClick={handleNav}>Projects</Link></li>
             <li><Link to="/Login" onClick={handleNav}>Login</Link></li>
             <li><Link to="/About" onClick={handleNav}>About</Link></li>
             <li>
